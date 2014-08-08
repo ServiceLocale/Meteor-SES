e9a5dc0fce8f82e223eb3bfc31a884a0e30268bd
Tarang Patel <tarangp@me.com>
2014-08-08 11:00:11 +0200

    Updates

--- package.js
+++ package.js
@@ -1,8 +1,10 @@
 Package.describe({
-  summary: 'Use Amazon SES for Email with Meteor'
+  summary: 'Use Amazon SES for Email with Meteor',
+  version: "1.0.0"
 });
 
 Package.on_use(function (api) {
+	if(api.versionsFrom) api.versionsFrom("METEOR-CORE@0.9.0-atm");
 	api.use(['meteor-nodemailer', 'email'], 'server');
 	api.imply(['meteor-nodemailer', 'email'], 'server');
 	api.add_files('lib.js', 'server');
